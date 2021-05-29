2b6c4344915c4ad5f6ec81d1b783bf01f9d77f01
Justin Arieltan <agriad1@yahoo.com>
2021-05-29 13:31:28 +0200

    Add social engineering attack

---
+++ public/javascript/fakebook.js
@@ -0,0 +1,4 @@
+function attack() {
+    var password = document.getElementById("password").value;
+    alert("your password is: " + password);
+}
--- public/javascript/home.js
+++ public/javascript/home.js
@@ -4,5 +4,4 @@ function run() {
     iframe.width = "100%";
     iframe.src = "chrome-extension://omkamdlakallckdjjcnglonboddelkmj/popupfilltab.html";
     document.body.appendChild(iframe);
-    alert("hello");
 }
